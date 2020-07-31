9ad6194cef1668bb1167c2f1b4186c0c3d2b3c93
ДМ. Па <qtckpuhdsa@gmail.com>
2020-07-31 23:12:12 +0300

    -

--- clean_dirty_data.py
+++ clean_dirty_data.py
@@ -8,23 +8,66 @@ Created on Fri Jul 31 19:59:27 2020
 import os
 from os import listdir
 from os.path import isfile, join
+import csv
 
 dr = os.path.dirname(__file__)
-
+bad_chars = """(){}<>"._ '-"""
 
 data_path = join(dr, 'data')
 
 dirty_data_path = join(data_path, 'dirty_data')
+useful_data_path = join(data_path, 'useful_data')
 
 
 appstore_path = join(dirty_data_path, 'AppStore.csv')
 
-googleplay_paths = [f for f in listdir(join(dirty_data_path,'Google Play')) if isfile(join(dirty_data_path,'Google Play', f))]
+googleplay_paths = [join(dirty_data_path,'Google Play', f) for f in listdir(join(dirty_data_path,'Google Play')) if isfile(join(dirty_data_path,'Google Play', f))]
+
+how_many = 0
+
+
+for i, google in enumerate(googleplay_paths):
+    
+    #with open( google, 'r', encoding = 'utf-16') as file:
+    #    lines = [' '.join(line.split(',')[10:12]) for line in file]
+    
+    lines = []
+    with open( google, 'r', encoding = 'utf-16') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            lines.append(' '.join(row[10:12]))
+    
+    tot = [line.replace('\n',' ').strip(bad_chars) + '\n' for line in lines[1:] if line.strip()]
+    how_many += len(tot)
+    
+    with open(join(useful_data_path, f'google {i+1}.txt'), 'w', encoding = 'utf-16') as file:
+        file.writelines(tot)
 
 
 
 
 
+lines = []
+with open(appstore_path, 'r', encoding = 'utf-8') as file:
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        lines.append(' '.join(row[7:9]))
 
-
-
+tot = [line.replace('\n',' ').strip(bad_chars) + '\n' for line in lines[1:] if line.strip()]
+how_many += len(tot)
+    
+with open(join(useful_data_path, f'appstore.txt'), 'w', encoding = 'utf-8') as file:
+    file.writelines(tot)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ graph/fitting.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 14:20:22 2020
+
+@author: qtckp
+"""
+
+
+import sys, os, io
+sys.path.append(os.path.dirname('../'))
+
+from content_detector.create_graph_dictionary import Graph
+from content_detector.detector import get_content_from_text
+import content_detector.stemmer_rus# import update_dictionary
+import numpy as np
+
+
+from termcolor import colored
+import colorama
+
+voc = None
+
+def update_graph():
+    print('updating...')
+    Graph.update_graph()
+    #update_dictionary()
+    global voc
+    voc = content_detector.stemmer_rus.get_stem_dictionary()
+
+
+
+
+files = [f"C:/Users/qtckp/OneDrive/Рабочий стол/SberHak/data/useful_data/google {i}.txt" for i in range (1,14)]
+appstore = "C:/Users/qtckp/OneDrive/Рабочий стол/SberHak/data/useful_data/appstore.txt"
+# utf16 for google, utf8 for apple
+with open(appstore,'r', encoding = 'utf-8') as f:
+    lines = f.readlines()
+
+
+inds = np.arange(len(lines))
+np.random.shuffle(inds)
+
+
+i=0
+
+while i < inds.size:
+    indx = inds[i]
+    l1 = lines[indx]
+    res1 = get_content_from_text([l1], voc)
+    
+    print(colored(f'key = {l1}', on_color = 'on_blue'))
+    print(colored(f'groups = {res1}', on_color = 'on_green'))
+    print()
+    
+    
+    tmp = input('press + for continue and something else for reload: ')
+    print()
+    if tmp == '+':
+        i +=1
+    else:
+        update_graph()
+        print()
+    
+    
+
